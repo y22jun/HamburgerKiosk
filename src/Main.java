1f79e76ee3608aba6906c7f4a8cfc6f8a3a42b81
y22jun <tlsdpwns0612@naver.com>
2025-03-04 18:06:47 +0900

    Feat:사용자 입력 기능 구현.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import io.Input;
+
 public class Main {
     public static void main(String[] args) {
-
+        String input = Input.nextLine();
+        System.out.println(input);
+        Input.close();
     }
 }
