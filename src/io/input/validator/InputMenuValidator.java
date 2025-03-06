592db28eaa291d32e252292dcbb39626ace383bb
y22jun <tlsdpwns0612@naver.com>
2025-03-06 17:34:54 +0900

    Feat:숫자가 0~4범위 검증하는 로직 구현.

--- src/io/input/validator/InputMenuValidator.java
+++ src/io/input/validator/InputMenuValidator.java
@@ -1,4 +1,16 @@
 package io.input.validator;
 
+import static io.input.validator.InputMenuErrorMessage.INVALID_NUMBER_INPUT;
+
 public class InputMenuValidator {
+
+    private static final int ZERO = 0;
+    private static final int FOUR = 4;
+
+    private void validateInvalidNumber(int inputNumber) {
+        if (inputNumber < ZERO || inputNumber > FOUR) {
+            throw new IllegalArgumentException(INVALID_NUMBER_INPUT.getMessage());
+        }
+    }
+
 }
