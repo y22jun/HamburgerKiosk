927c98fc26523896c6988ddb64307114917bf313
y22jun <tlsdpwns0612@naver.com>
2025-03-08 18:13:40 +0900

    Feat:파일 예외처리 검증 로직 구현.

---
+++ src/main/java/file/LoadProductsFile.java
@@ -0,0 +1,4 @@
+package main.java.file;
+
+public class LoadProductsFile {
+}
--- src/main/java/file/validator/LoadProductsFileErrorMessage.java
+++ src/main/java/file/validator/LoadProductsFileErrorMessage.java
@@ -1,9 +1,11 @@
 package main.java.file.validator;
 
 public enum LoadProductsFileErrorMessage {
+    FILE_NULL("[ERROR] 파일이 null 입니다."),
+    FILE_NOT_EXISTS("[ERROR] 파일이 존재하지 않습니다."),
     FILE_READ_ERROR("[ERROR] 파일을 읽는 중 오류가 발생했습니다."),
     FILE_WRITE_ERROR("[ERROR] 파일을 쓰는 중 오류가 발생했습니다."),
-            ;
+    ;
 
     private final String message;
 
