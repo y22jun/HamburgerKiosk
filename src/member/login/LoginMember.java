16d7ad0298b18bd8609ea79caaf65f0ba78fdd79
y22jun <tlsdpwns0612@naver.com>
2025-03-08 16:35:12 +0900

    Feat:멤버 로그인 예외처리 기능 구현.

--- src/member/login/LoginMember.java
+++ src/member/login/LoginMember.java
@@ -3,6 +3,7 @@ package member.login;
 import io.input.Input;
 import member.Member;
 import member.create.CreateMember;
+import member.login.validator.LoginMemberValidator;
 
 public class LoginMember {
 
@@ -10,9 +11,13 @@ public class LoginMember {
         System.out.println("회원 아이디를 입력해주세요.");
         String inputLoginMember = Input.nextLine();
 
-        Member createMember = CreateMember.getMember();
-        if (createMember.getMemberId() == Integer.parseInt(inputLoginMember)) {
+        try {
+            LoginMemberValidator.validateLogin(inputLoginMember);
+
+            Member createMember = CreateMember.getMember();
             System.out.println("로그인 성공! " + createMember.getMemberId() + "님 환영합니다.");
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
         }
     }
 }
