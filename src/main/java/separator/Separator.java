7bd7e3312633c805fadb2f47c06ca3bd57454917
y22jun <tlsdpwns0612@naver.com>
2025-03-18 20:26:00 +0900

    Fix: 회원 로그인 버그 수정.

--- src/main/java/separator/Separator.java
+++ src/main/java/separator/Separator.java
@@ -5,7 +5,5 @@ public class Separator {
     public static final String COMMA = ",";
     public static final char LEFT_SQUARE_BRACKET = '[';
     public static final char RIGHT_SQUARE_BRACKET = ']';
-    public static final char HYPHEN = '-';
-    public static final char DOUBLE_QUOTES = '"';
 
 }
